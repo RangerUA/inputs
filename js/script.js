f97b1decc80b32d432991f46c0d37999b5611e90
Roman Kopylov <ramzess.zvir@gmail.com>
2017-11-02 17:04:23 +0200

    remove counter

--- js/script.js
+++ js/script.js
@@ -6,26 +6,22 @@ container.addEventListener('click', function createPatern(e){
 	let elemPrev = e.target.parentNode.previousElementSibling;
 	let elemNext = e.target.parentNode.nextElementSibling;
 	let patern = `<div class="field">
-      <span>${containerCounter()}</span>
-      <input type="text" data-name="${containerCounter()}">
+      <input type="text">
       <button data-action="plus">+</button>
       <button data-action="minus">-</button>
     </div>`;
-    console.log(elemNext);
 
 	if(attr == 'plus'){
 		container.insertAdjacentHTML('beforeEnd', patern);
 	}
-	if(attr == 'minus' && elemNext != null || elemPrev != null){
+
+	if(attr == 'minus' && (elemNext != null || elemPrev != null)){
 		containerCounter();
 		e.target.parentNode.classList.add('opacity');
 		setTimeout(function(){
 			e.target.parentNode.remove();
-			console.log(containerCounter() - 1);
 		}, 500);
 	}
-
-	return;
 
 });
 
